80322e67f9a18f960fb2a2b81f6ef45c637cb1c3
Georges34 <georges.saifi2001@gmail.com>
2024-11-04 16:57:07 +0200

    Update functions.py
    Relativistic Pusher

--- modules/functions.py
+++ modules/functions.py
@@ -18,6 +18,7 @@ def update_v(
     steps: float,
     q=1.6e-19,
     m=9.11e-31,
+    c=3e8
 ):
     s = np.zeros((3, steps))
     T = ((q * dt) / (2 * m)) * B
@@ -28,10 +29,13 @@ def update_v(
     
     for i in tqdm(range(0, steps - 1)):
 
+        v_mag = np.linalg.norm(v[:, i])
+        gamma = 1 / np.sqrt(1 - (v_mag**2 / c**2))
+        T[:, i] = ((q * dt) / (2 * gamma * m)) * B[:, i]
         s[:, i] = (2 / (1 + np.linalg.norm(T[:, i]) ** 2)) * T[:, i]
-        vm[:, i] = v[:, i] + (q / m) * E[:, i] * dt * 0.5
+        vm[:, i] = v[:, i] + (q /(gamma* m)) * E[:, i] * dt * 0.5
         v_aux[:, i] = vm[:, i] + np.cross(vm[:, i], T[:, i])
         vp[:, i] = vm[:, i] + np.cross(v_aux[:, i], s[:, i])
-        v[:, i + 1] = vp[:, i] + (q / m) * E[:, i] * 0.5 * dt
+        v[:, i + 1] = vp[:, i] + (q / (gamma*m)) * E[:, i] * 0.5 * dt
 
     return v
