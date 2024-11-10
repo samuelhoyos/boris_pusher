1da6f1febbddfab476541acdfb50159fccdec61c
Daniel PERALES RIOS <danielperales2202@gmail.com>
2024-11-10 18:28:21 +0100

    Plotting the eta-zeta graph

--- modules/rkg_modified.py
+++ modules/rkg_modified.py
@@ -10,11 +10,11 @@ beta_p = 0.2
 
 
 def rkg_vector(
-    func: Callable[[float, np.ndarray,int], np.ndarray],  # Updated function signature
-    x_initial: float,
+    func: Callable[[float, np.ndarray,int], np.ndarray],  # The particle dynamics function
+    x_initial: float, # Initial time for the simulation
     state_initial: np.ndarray,
-    step_size: float,
-    x_final: float,
+    step_size: float, # Step size for the simulation
+    x_final: float, # Final time for the simulation
 ) -> np.ndarray:
     
     if x_initial >= x_final:
@@ -23,14 +23,14 @@ def rkg_vector(
     if step_size <= 0:
         raise ValueError("Step size must be positive.")
     
-    n = int((x_final - x_initial) / step_size)
-    states = np.zeros((n + 1, len(state_initial)))
-    states[0] = state_initial
+    n = int((x_final - x_initial) / step_size) # Number of iterations
+    states = np.zeros((n + 1, len(state_initial))) # For each iteration, states contains the values of the parameters at each timestep
+    states[0] = state_initial # Initial state of the parameters (n rows)
     times = np.zeros(n + 1)  # Array to store the time (t) values
     times[0] = x_initial
 
     
-    s = x_initial
+    s = x_initial # "s" for state
     for i in range(n):
         try:
             k1 = step_size * func(s, states[i],i)
@@ -52,18 +52,16 @@ def rkg_vector(
     return states
 
 
-def particle_dynamics(t, state,iteration=None):
-    beta_x, beta_y, beta_z, zeta = state
+def particle_dynamics(t, state, iteration=None):
+    beta_x, beta_y, beta_z, zeta, eta_1_ghost, eta_2_shost, zeta_ghost = state
     beta = np.sqrt(beta_x**2 + beta_y**2 + beta_z**2)
     phi=0
     if beta >= 1:
         print("Last valid state", state)
         raise ValueError(f"Error at iteration {iteration}: Beta squared is greater than or equal to 1, which makes gamma invalid.")
 
-
-
     gamma = 1/np.sqrt(1-beta**2)
-    eta_1 = beta_p * t - alpha * zeta**2 +phi
+    eta_1 = beta_p * t - alpha * zeta**2 + phi
     eta_2 = -eta_1
 
     # Compute magnetic and electric field components
@@ -82,16 +80,16 @@ def particle_dynamics(t, state,iteration=None):
     Bz_t = (B_z1 + B_z2)
     
     # Equations of motion
-    d_beta_x = 1/gamma * (Ex_t + (1 - beta_x**2) + (beta_y* Bz_t) - (beta_z * By_t))
+    d_beta_x = 1/gamma * (Ex_t + (1 - beta_x**2) + (beta_y * Bz_t) - (beta_z * By_t))
     # print( "E_x", Ex_t)
     # print( "B_z", Bz_t)
     # print( "B_y", By_t)
     # print( "d_beta_x", d_beta_x)
-    d_beta_y = -beta_x / gamma * (Bz_t + Ex_t*beta_y) 
-    d_beta_z = -beta_x / gamma * (By_t + Ex_t*beta_z)
+    d_beta_y = -beta_x / gamma * (Bz_t + Ex_t * beta_y) 
+    d_beta_z = -beta_x / gamma * (By_t + Ex_t * beta_z)
     d_zeta = beta_z
     
-    return np.array([d_beta_x, d_beta_y, d_beta_z, d_zeta])
+    return np.array([d_beta_x, d_beta_y, d_beta_z, d_zeta, eta_1, eta_2, zeta])
 
 
 # Parameters for the dynamic
--- scripts/run_rkg.py
+++ scripts/run_rkg.py
@@ -1,8 +1,13 @@
-from modules import functions, rkg_modified
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
+from modules import rkg_modified
+import matplotlib.pyplot as plt
 
 
-N=100
+
+N=50
 q = 3e9
 me = 9.11e-28
 alpha = 0.05
@@ -13,8 +18,8 @@ beta_p = 0.2
 # zeta_0 = np.random.uniform(-10, 10,size=N)
 
 tau_initial=0
-tau_final=10
-step_size=0.01
+tau_final=1000
+step_size=0.001
 # beta_x0 = np.zeros(N)
 # beta_y0 = -beta_p/4 * (np.tanh(eta_1_0) + np.tanh(eta_2_0) * (np.tanh(eta_1_0 -eta_2_0 - 2)))
 
@@ -24,24 +29,25 @@ step_size=0.01
 initial_state=[]
 
 results = []
+eta_total_array = []
+zeta_total_array = []
 if __name__ == "__main__":
     for i in range(N):
         np.random.seed(i)  # Use the loop index or another varying number as the seed
+        right_or_left = False
 
         # Generate initial conditions for each iteration
         eta_1_0 = np.random.uniform(-10, 10)
         eta_2_0 = np.random.uniform(-10, 10)
         zeta_0 = np.random.uniform(-10, 10)
         
-        
         beta_x0 = 0
         beta_y0 = -beta_p / 4 * (np.tanh(eta_1_0) + np.tanh(eta_2_0) * (np.tanh(eta_1_0 - eta_2_0 - 2)))
         beta_z0 = -beta_p * alpha * zeta_0 / 2 * (np.tanh(eta_1_0) - np.tanh(eta_2_0) - 2)
 
-        initial_state = np.array([beta_x0, beta_y0, beta_z0, zeta_0])
+        initial_state = np.array([beta_x0, beta_y0, beta_z0, zeta_0, eta_1_0, eta_2_0, zeta_0])
 
-        
-        try:
+        try:            
             results.append(rkg_modified.rkg_vector(
                 func=rkg_modified.particle_dynamics,
                 x_initial=tau_initial,
@@ -49,6 +55,26 @@ if __name__ == "__main__":
                 step_size=step_size,
                 x_final=tau_final
             ))
+
+            eta_array = np.array([]) # The arrays are emptied after each iteration
+            zeta_array = np.array([])
+
+            if eta_1_0 > eta_2_0: # If the particle was going to the forward direction
+                eta_array = results[-1][0][:,4]
+            else: # If the particle was going to the backward direction
+                eta_array = + results[-1][0][:,5]
+
+            zeta_array = results[-1][0][:,6]            
+            eta_total_array.append(eta_array)
+            zeta_total_array.append(zeta_array)
+
         except ValueError as e:
             print(f"Error encountered: {e}")
+    
+    for i in range(len(eta_total_array)):
+        plt.plot(eta_total_array[i], zeta_total_array[i])
+
+    plt.xlabel("Eta")
+    plt.ylabel("Zeta")
+    plt.show()
 
