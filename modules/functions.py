b84787a74545dd630d61bb2740d4404d902fec0b
Samuel <samuel.hoyos@udea.edu.co>
2024-12-12 17:06:23 +0100

    Boris Pusher to make it compatible with Takeushi

--- modules/functions.py
+++ modules/functions.py
@@ -4,11 +4,10 @@ from tqdm import tqdm
 
 
 
-def update_x(v: np.array, x: np.array, dt: float, steps: float) -> np.array:
+def update_r(v: np.array, r: np.array, dt: float, steps: float) -> np.array:
     for i in tqdm(range(0, steps - 1)):
-        x[:, i + 1] = x[:, i] + v[:, i] * dt
-    return x
-
+        r = r + v*dt
+    return r
 
 def update_v_relativistic(
     v: np.array,
@@ -16,26 +15,28 @@ def update_v_relativistic(
     B: np.array,
     dt: float,
     steps: float,
-    q=1.6e-19,
-    m=9.11e-31,
-    c=3e8
+    q:float=1.6e-19,
+    m:float=9.11e-31,
+    c:float=3e8
 ):
-    s = np.zeros((3, steps))
+    s = np.zeros(3)
     T = ((q * dt) / (2 * m)) * B
     # s = (2 / (1 + np.linalg.norm(T, axis=0) ** 2)) * T
-    vm = np.zeros((3, steps))
-    vp = np.zeros((3, steps))
-    v_aux = np.zeros((3, steps))
+    vm = np.zeros(3)
+    vp = np.zeros(3)
+    v_aux = np.zeros(3)
     
     for i in tqdm(range(0, steps - 1)):
 
-        v_mag = np.linalg.norm(v[:, i])
+        v_mag = np.linalg.norm(v)
         gamma = 1 / np.sqrt(1 - (v_mag**2 / c**2))
-        T[:, i] = ((q * dt) / (2 * gamma * m)) * B[:, i]
-        s[:, i] = (2 / (1 + np.linalg.norm(T[:, i]) ** 2)) * T[:, i]
-        vm[:, i] = v[:, i] + (q /(gamma* m)) * E[:, i] * dt * 0.5
-        v_aux[:, i] = vm[:, i] + np.cross(vm[:, i], T[:, i])
-        vp[:, i] = vm[:, i] + np.cross(v_aux[:, i], s[:, i])
-        v[:, i + 1] = vp[:, i] + (q / (gamma*m)) * E[:, i] * 0.5 * dt
+        T = ((q * dt) / (2 * gamma * m)) * B
+        s = (2 / (1 + np.linalg.norm(T) ** 2)) * T
+        vm = v + (q /(gamma* m)) * E * dt * 0.5
+        v_aux = vm + np.cross(vm, T)
+        vp = vm + np.cross(v_aux, s)
+        v = vp + (q / (gamma*m)) * E * 0.5 * dt
 
     return v
+
+
