340fb4c174e9204b7ddce272c526202eefcb2023
Samuel <samuel.hoyos@udea.edu.co>
2024-12-14 00:41:25 +0100

    Boris Pusher Correction

--- modules/functions.py
+++ modules/functions.py
@@ -2,32 +2,34 @@ import numpy as np
 from tqdm import tqdm
 
 
-def update_r(v: np.array, r: np.array, dt: float, steps: float) -> np.array:
+def update_r(v: np.array, r: np.array, dt: float) -> np.array:
     return r + v*dt
 
 def update_v_relativistic(
     v: np.array,
     E: np.array,
     B: np.array,
-    vm: np.array,
-    vp: np.array,
-    v_aux: np.array,
     dt: float,
     q:float=1.609e-19,
     m:float=9.11e-31,
     c:float=3e8,
 ):
-    s = np.zeros(3)
-    T = ((q * dt) / (2 * m)) * B
-    # s = (2 / (1 + np.linalg.norm(T, axis=0) ** 2)) * T
+    
 
     v_mag = np.linalg.norm(v)
     gamma = 1 / np.sqrt(1 - (v_mag*2 / c*2))
     T = ((q * dt) / (2 * gamma * m)) * B
     s = (2 / (1 + np.linalg.norm(T) ** 2)) * T
-    vm = v + (q /(gamma* m)) * E * dt * 0.5
+
+    
+    vm = v + ((q /(gamma* m)) * E * dt * 0.5)
     v_aux = vm + np.cross(vm, T)
     vp = vm + np.cross(v_aux, s)
-    v = vp + (q / (gamma*m)) * E * 0.5 * dt
 
-    return v, vm, vp, v_aux
+
+    v_mag = np.linalg.norm(vp)  
+    gamma = 1 / np.sqrt(1 - (v_mag**2 / c**2))
+    v = vp + (q / (gamma * m)) * E * (0.5 * dt)
+    
+
+    return v
---
+++ scripts/test_boris.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+from modules import functions
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+initial_v=np.array([1,0,0])
+initial_r=np.array([0,0,0])
+E=np.array([0.1,0.05,0])
+B=np.array([1,1,1])
+dt=(0.01*9.11e-31)/(1.602e-19*np.linalg.norm(B))
+times=np.arange(0,1e-10,dt)
+velocity=np.zeros((len(times),3))
+position=np.zeros((len(times),3))
+energy=np.zeros(len(times))
+for i, t in enumerate(times):
+    if i==0:
+        velocity[i]=initial_v
+        position[i]=initial_r
+
+    else:
+        velocity[i]=(functions.update_v_relativistic(v=velocity[i-1],E=E,B=B,dt=dt))
+        position[i]=functions.update_r(v=velocity[i],r=position[i-1],dt=dt)
+    energy[i]=0.5*9.11e-31*np.linalg.norm(velocity[i],ord=2)
+
+
+fig = plt.figure()
+plt.axes(projection='3d').scatter(position[:,0],position[:,1], position[:,2])
+print(energy)
+
+plt.show()
